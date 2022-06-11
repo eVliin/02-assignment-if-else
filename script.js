074de92c1fde3d2f51d7d09d493137d6ca8a6291
eVliin <evelynma.b7@gmail.com>
2022-06-11 19:22:58 -0300

    quase la

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let name = "evelyn"
+let secName = "lima"
+let age = 17
+let birthday = 10
+let Human = true
+
+if (age >= 18 && Human) {
+    console.log("Humano")
+} else {
+    console.log("cachorro? et? criança?")
+}
+if (age >= 18) {
+    console.log("desempregado")
+} else {
+    console.log("de férias😎")
+}
+if(birthday == 1 || birthday == 12){
+    console.log("aniversário ta lonje ainda ;-;")
+}
+if(name.substr(0,1) == "R"){
+    console.log("Gangue do R")
+}else{
+    console.log("não é da gangue do r >:(")
+}
